e0e1f10ae944b0c004ae635a36a23bc27a9ebf60
pfelto <paul.r.felton@gmail.com>
2022-01-07 11:31:40 -0500

    add a bit more functionality to the Vanilla Js app to have a form that on submit you add the input value to an ul. Also uninstalled eslint for now as ts in visual code shows your errors.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const button = document.getElementById('counter');
+const form = document.querySelector('form');
 
-button.addEventListener('click',()=>{console.log('click')})
+
+function saveInput(e){
+    e.preventDefault();
+    const itemValue = document.querySelector('input').value
+    if(itemValue === ''){
+        return;
+    }
+    const node = document.createElement("LI");                 // Create a <li> node
+    const textnode = document.createTextNode(itemValue);         // Create a text node
+    node.appendChild(textnode);                              // Append the text to <li>
+    document.getElementById("list").appendChild(node);     // Append <li> to <ul> with id="myList"
+    form.reset();
+}
+
+form.addEventListener("submit", (e) => saveInput(e))
